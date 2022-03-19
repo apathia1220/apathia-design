439fd291c43a30f8b6dc035821e687d7cdcff561
xzc <1343129638@qq.com>
2022-03-19 22:09:49 +0800

    add

--- components/Tabs/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { TabProps } from './tabs';
-import { TabsItemProps } from './tabsItem';
-declare type TabsComponent = React.FC<TabProps> & {
-    Item?: React.FC<TabsItemProps>;
-};
-declare let TransTab: TabsComponent;
-export default TransTab;
--- components/Tabs/index.js
+++
@@ -1,6 +0,0 @@
-import Tabs from './tabs';
-import TabsItem from './tabsItem';
-var TransTab = Tabs;
-TransTab.Item = TabsItem;
-// export type {TabProps, TabsItemProps}
-export default TransTab;
